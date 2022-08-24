d0986977826a327f46f12ca00c39977c34842607
Gabriel Andrade <gabriel.santana@gmail.com>
2022-08-24 05:20:33 -0300

    Realizando a inclusão de registro no firestore

--- android/app/src/main/java/com/myshopping/MainActivity.java
+++ android/app/src/main/java/com/myshopping/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myshopping;
+package com.tasklistTeste;
 
 import android.os.Bundle;
 
--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { Alert } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
 
 import { Container } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
 
+
+
 export function FormBox() {
+  const [description,setDescription] = useState('');
+  const [quantity,setQuantity] = useState(0);
+
+  async function handleProductAdd(){
+    firestore()
+    .collection('products')
+    //.doc('') Inclusão de um Id PErsonalizado
+    //set({ => Para o Id Customizado substituir o add pelo set
+    .add({
+      description,
+      quantity,
+      done:false
+    })
+    .then(() => {
+      Alert.alert('Produto adicionado com sucesso');
+    })
+    .catch((error) => console.log(error))
+  }
+
   return (
     <Container>
       <Input
         placeholder="Nome do produto"
         size="medium"
+        onChangeText={setDescription}
       />
 
       <Input
@@ -17,12 +42,13 @@ export function FormBox() {
         keyboardType="numeric"
         size="small"
         style={{ marginHorizontal: 8 }}
+        onChangeText={value => setQuantity(Number(value))}
       />
 
       <ButtonIcon
         size='large'
         icon="add-shopping-cart"
-        onPress={() => { }}
+        onPress={handleProductAdd}
       />
     </Container>
   );
